8f183b8de30894788480d53bfdee9ce806ab6344
Benjamin Milde <benni@kobrakai.de>
2016-07-14 10:33:25 +0200

    Test Commit

---
+++ FieldtypeCountrySelectConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+class FieldtypeCountrySelectConfig extends ModuleConfig {
+	public function getDefaults() {
+		return array(
+		);
+	}
+
+	public function getInputfields() {
+		$inputfields = parent::getInputfields();
+
+		return $inputfields; 
+	}
+}
